32aae13e0d41cf4cf3be2e7f2964df9239785588
PRYANIK48 <112880306+PRYANIK48@users.noreply.github.com>
2026-05-30 22:27:07 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <string>
+#include <iostream>
+
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
 
 int main() {
+    BST<std::string> tree;
+    const char* filename = "war_peace.txt";
+    makeTree(tree, filename);
+    printFreq(tree);
     return 0;
 }
